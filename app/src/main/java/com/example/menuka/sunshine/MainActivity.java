0843d65419461ee79ad61a925c1f6176132ac22b
Menuka <menuka.14@cse.mrt.ac.lk>
2017-01-14 23:25:25 +0530

    Add dummy data :: implement adapter

--- app/src/main/java/com/example/menuka/sunshine/MainActivity.java
+++ app/src/main/java/com/example/menuka/sunshine/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.menuka.sunshine;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +21,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public static class PlaceholderFragment extends Fragment{
+
+        ArrayAdapter<String> mForecastAdapter;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            String[] forecastArray = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17"
+                    ,
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+
+            List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
+            // create an ArrayAdapter
+            // use it to populate the ListView attached to
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(), // The current context (this activity)
+                    R.layout.list_item_forecast, // The name of the layout ID
+                    R.id.list_item_forecast_textview,   // The ID of the textview to populate
+                    weekForecast
+            );
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Get a reference to the ListView, and attach this adapter to it
+            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
+            return rootView;
+        }
+    }
+
 }
